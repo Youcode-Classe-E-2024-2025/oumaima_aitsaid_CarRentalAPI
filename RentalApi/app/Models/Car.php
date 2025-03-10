76634f140fd04bd84c873be018778dbd2ba335c7
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-03-10 14:16:48 +0000

    add model cars

--- RentalApi/app/Models/Car.php
+++ RentalApi/app/Models/Car.php
@@ -1,10 +1,33 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable= [
+        'brand',
+        'model',
+        'license_plate',
+        'year',
+        'color',
+        'transmission',
+        'fuel_type',
+        'seats',
+        'daily_rate',
+        'is_available',
+        'description',
+        'image',
+    ];
+    protected $casts = [
+        'is_available' => 'boolean',
+        'daily_rate' => 'decimal:2',
+        'year' => 'integer',
+    ];
+    public function rentals()
+    {
+        return $this->hasMany(Rental::class);
+    }
 }
