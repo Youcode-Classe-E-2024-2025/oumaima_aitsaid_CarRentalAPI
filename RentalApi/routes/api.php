98fd2389f73a1c94c1f5364bab183e1c3d1ed72c
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-03-10 15:05:01 +0000

    Define API routes fro cars ,rentals,payments

--- RentalApi/routes/api.php
+++ RentalApi/routes/api.php
@@ -3,12 +3,30 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CarController;
+use App\Http\Controllers\API\RentalController;
+use App\Http\Controllers\API\PaymentController;
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+// Public car routes
+Route::get('/cars', [CarController::class, 'index']);
+Route::get('/cars/{id}', [CarController::class, 'show']);
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
+    // Car routes (admin only)
+    Route::post('/cars', [CarController::class, 'store']);
+    Route::put('/cars/{id}', [CarController::class, 'update']);
+    Route::delete('/cars/{id}', [CarController::class, 'destroy']);
+    
+    // Rental routes
+    Route::apiResource('rentals', RentalController::class);
+    
+    // Payment routes
+    Route::apiResource('payments', PaymentController::class);
 });
